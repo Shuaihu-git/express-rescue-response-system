ed95da3c0cec3e995e664cc499b8143d20150c46
XiaoHu <1583139786@qq.com>
2024-12-04 08:45:08 +0800

    fix:更新相关提示

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -24,6 +24,6 @@ public class SysIndexController
     @RequestMapping("/")
     public String index()
     {
-        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", ruoyiConfig.getName(), ruoyiConfig.getVersion());
+        return StringUtils.format("高速公路快速救援响应系统，请通过前端地址访问。");
     }
 }
